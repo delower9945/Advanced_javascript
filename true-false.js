89754899afe0c7e1ac3370b00a5c56a060d64f01
delower9945 <delowerweb99@gmail.com>
2021-02-10 22:30:15 -0800

    tru-fals comparishion

---
+++ true-false.js
@@ -0,0 +1,20 @@
+/**  False Valu
+0
+""
+null
+undefined
+NaN
+True Value start-----
+"0", " " , {}, []
+*/
+
+// const name = 6;
+// const name = "hsankhan"
+
+const name = null;
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
